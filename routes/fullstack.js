f8a99e19b987f2d2a0d2e0114c6ef4b4522cb702
Mike Gossmann <mgossmann@labx.com>
2020-08-13 15:34:17 -0400

    Add the description for the Fullstack test

--- routes/fullstack.js
+++ routes/fullstack.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("pages/fullstack", {});
+    res.render("pages/fullstack", {title: "Fullstack"});
 });
 
 module.exports = router;
